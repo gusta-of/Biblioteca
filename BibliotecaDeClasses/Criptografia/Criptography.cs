241004103d8b56e4e47e85c1f5417d9954232cd4
Gustavo Fernandes <Gustavo Fernandes@DESKTOP-LSM249T>
2019-07-10 23:19:37 -0300

    Ajuste commit

--- BibliotecaDeClasses/Criptografia/Criptography.cs
+++ BibliotecaDeClasses/Criptografia/Criptography.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace FernandesDLL.Model
+namespace BibliotecaDeClasses.Criptografia
 {
     public class Criptography
     {
