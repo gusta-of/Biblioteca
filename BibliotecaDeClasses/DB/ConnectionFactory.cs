636a2fa50fa49c82d416a625e02d8a6c8e9f23d1
Gustavo Fernandes <Gustavo Fernandes@DESKTOP-LSM249T>
2019-07-13 14:23:38 -0300

    Cria dll de conexão com o banco de dados

---
+++ BibliotecaDeClasses/DB/ConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasses.DB
+{
+    public class ConnectionFactory
+    {
+        private static MySqlConnection _conexao = null;
+
+        public static MySqlConnection Connetion()
+        {
+            try
+            {
+                var server = "server=localhost";
+                var porta = "port=3306";
+                var userid = "User id=root";
+                var password = "password=root";
+                var database = "database=aplicacao_desck";
+                _conexao = new MySqlConnection($"{server};{porta};{userid};{database}; {password}");
+            }
+            catch (MySqlException e)
+            {
+            }
+
+            return _conexao;
+        }
+    }
+}
