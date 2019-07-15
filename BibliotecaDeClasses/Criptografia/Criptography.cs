330ccba13310147e167353b1e35edb0612aa137b
Gustavo Fernandes <Gustavo Fernandes@DESKTOP-LSM249T>
2019-07-14 21:17:05 -0300

    resolve conflit

--- BibliotecaDeClasses/Criptografia/Criptography.cs
+++ BibliotecaDeClasses/Criptografia/Criptography.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace BibliotecaDeClasses.Criptografia
+namespace FernandesDLL.Model
 {
     public class Criptography
     {
