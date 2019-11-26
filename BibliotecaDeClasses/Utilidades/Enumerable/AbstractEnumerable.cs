a8f3973bf4dd56ee833371eb05068265ae07824b
Fernandes <gustavo.fernandes@escolarmanager.com.br>
2019-11-26 13:44:15 -0300

    Cria utilitário enumerador abstrato

---
+++ BibliotecaDeClasses/Utilidades/Enumerable/AbstractEnumerable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasses.Utilidades.Enumerable
+{
+    public abstract class AbstractEnumerable<T, K> : IEnumerable<K>, IComparable where T : IEnumerable<K>
+    {
+        private Func<string> _descricaoDelegate;
+        protected AbstractEnumerable(K key, Func<string> descricao)
+        {
+            Codigo = key;
+            _descricaoDelegate = descricao;
+        }
+
+        public K Codigo { get; protected set; }
+
+        public string Descricao
+        {
+            get
+            {
+                return _descricaoDelegate();
+            }
+            protected set
+            {
+                _descricaoDelegate = () => value;
+            }
+        }
+
+        public T Get(T key)
+        {
+            return GetAll().Find(i => i.Codigo.Equals(key));
+        }
+
+        public List<T> GetAll()
+        {
+            return GetAll<T>();
+        }
+
+        protected static List<TEnumerador> GetAll<TEnumerador>() where TEnumerador : IEnumerable<K>
+        {
+            var tipo = typeof(TEnumerador);
+            return tipo.GetFields(BindingFlags.Static | BindingFlags.Public).Select(campo => (TEnumerador)campo.GetValue(null)).ToList();
+        }
+
+        public bool Equals(AbstractEnumerable<T, K> obj)
+        {
+            return (obj != null) && obj.Codigo.Equals(Codigo);
+        }
+
+        public override int GetHashCode()
+        {
+            return Codigo.GetHashCode();
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+           if(!(obj is AbstractEnumerable<T, K>))
+            {
+                throw new ArgumentException("Objeto não é do tipo do Enumerador");
+            }
+
+            return ((IComparable)Codigo).CompareTo(((AbstractEnumerable<T, K>)obj).Codigo);
+        }
+    }
+}
---
+++ BibliotecaDeClasses/Utilidades/Enumerable/IEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasses.Utilidades.Enumerable
+{
+    public interface IEnumerable<T>
+    {
+        T Codigo { get; }
+        string Descricao { get; }
+    }
+}
